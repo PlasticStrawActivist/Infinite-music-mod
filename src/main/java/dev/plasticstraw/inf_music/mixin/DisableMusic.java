38ac0de49f7913fecf7ebdc9a07458cef559c7df
PlasticStrawActivist <aiden.landy@gmail.com>
2024-02-18 20:45:57 +1000

    remove old code

--- src/main/java/dev/plasticstraw/inf_music/mixin/DisableMusic.java
+++ src/main/java/dev/plasticstraw/inf_music/mixin/DisableMusic.java
@@ -13,44 +13,6 @@ import net.minecraft.sound.MusicSound;
 @Mixin(MusicTracker.class)
 public class DisableMusic {
 
-    // @Shadow
-    // private SoundInstance current;
-
-    // @Inject(method = "Lnet/minecraft/client/sound/MusicTracker;tick()V", at =
-    // @At("HEAD"),
-    // cancellable = true)
-    // private void disable(CallbackInfo ci) {
-    // MinecraftClient client = MinecraftClient.getInstance();
-    // MusicSound musicSound = client.getMusicType();
-
-    // if (!musicSound.enabled()) {
-    // ci.cancel();
-    // }
-    // }
-
-    // @Redirect(method = "Lnet/minecraft/client/sound/MusicTracker;tick()V",
-    // at = @At(value = "INVOKE", target = "Ljava/lang/Math;min(II)I", ordinal = 0))
-    // private int resetTimeUntilNextSong(int arg1, int arg2) {
-    // return arg2;
-    // }
-
-    // @Redirect(
-    // method =
-    // "Lnet/minecraft/client/sound/MusicTracker;play(Lnet/minecraft/sound/MusicSound;)V",
-    // at = @At(value = "INVOKE",
-    // target =
-    // "Lnet/minecraft/client/sound/SoundManager;play(Lnet/minecraft/client/sound/SoundInstance;)V"))
-    // private void pauseForMusicMusic(SoundManager soundManager, SoundInstance
-    // sound) {
-    // if (InfiniteMusic.CONFIG.pauseForDiscMusic &&
-    // InfiniteMusic.isMusicDiscMusicPlaying()) {
-    // return;
-    // }
-
-    // InfiniteMusic.musicInstance = this.current;
-    // soundManager.play(sound);
-    // }
-
     @Inject(method = "Lnet/minecraft/client/sound/MusicTracker;tick()V", at = @At("HEAD"), cancellable = true)
     private void redirectTick(CallbackInfo ci) {
         ci.cancel();
